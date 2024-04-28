6948330e9a945f2fa2f2f8faf9dd2302e9867b62
Froggit2 <165292357+Froggit2@users.noreply.github.com>
2024-04-28 18:12:24 +0300

    Add files via upload

---
+++ New_File.py
@@ -0,0 +1,4 @@
+def hello_world_1():
+    print('Hello world')
+def hello_world_2():
+    print('Hello world')
---
+++ main.py
@@ -0,0 +1,3 @@
+from New_File import hello_world_1 as H_w_1, hello_world_2 as H_w_2
+H_w_1()
+H_w_2()
